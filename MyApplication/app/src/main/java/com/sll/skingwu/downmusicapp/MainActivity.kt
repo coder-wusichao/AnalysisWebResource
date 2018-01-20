a856dcb0ab787059267b1f0d2158f445a0907623
wusichao <wusichao@163.com>
2018-01-20 18:41:53 +0800

    fist commit

---
+++ MyApplication/app/src/main/java/com/sll/skingwu/downmusicapp/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.sll.skingwu.downmusicapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.tencent.smtt.sdk.WebView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun analysData (view:View){
+
+
+
+    }
+}
